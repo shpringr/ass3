0a577f3825e74714d7d49e8be129d25dbb93d011
ROTEM <shpringr@post.bgu.ac.il>
2017-01-15 18:49:33 +0200

    yallaa

--- Server/src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
+++ Server/src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -8,20 +8,41 @@ import bgu.spl171.net.srv.bidi.ConnectionHandler;
 public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<T> {
 
     private boolean shouldTerminate = false;
-    Connections<T> connections;
+    private Connections<T> connections;
+    private int connectionId;
 
     @Override
     public void start(int connectionId, Connections<T> connections) {
-        //connections.add
-
+        this.connections = connections;
+        this.connectionId = connectionId;
     }
 
     @Override
     public void process(T message) {
         switch ((((Packets)message).getOpCode())){
+            //RRQ
+            case 1 :
+
+                break;
+            case 2 :
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
             case 10:
                 shouldTerminate = true;
-
+                break;
         }
 
     }
