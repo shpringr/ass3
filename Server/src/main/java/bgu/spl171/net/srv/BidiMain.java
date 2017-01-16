f26f7d24c53dee02a738b11e1aae2d564e108099
Orel Hazan <orelhaz@post.bgu.ac.il>
2017-01-17 00:13:16 +0200

    lovee

---
+++ Server/src/main/java/bgu/spl171/net/srv/BidiMain.java
@@ -0,0 +1,29 @@
+package bgu.spl171.net.srv;
+
+import bgu.spl171.net.api.MessageEncoderDecoderImpl;
+import bgu.spl171.net.api.bidi.BidiMessagingProtocolImpl;
+import bgu.spl171.net.impl.newsfeed.NewsFeed;
+
+/**
+ * Created by Orel Hazan on 17/01/2017.
+ */
+public class BidiMain {
+
+    public static void main(String[] args) {
+// you can use any server...
+        Server.threadPerClient(
+                7777, //port
+                BidiMessagingProtocolImpl::new, //protocol factory
+                MessageEncoderDecoderImpl::new //message encoder decoder factory
+        ).serve();
+
+//        Server.reactor(
+//                Runtime.getRuntime().availableProcessors(),
+//                7777, //port
+//                () ->  new RemoteCommandInvocationProtocol<>(feed), //protocol factory
+//                ObjectEncoderDecoder::new //message encoder decoder factory
+//        ).serve();
+
+    }
+
+}
